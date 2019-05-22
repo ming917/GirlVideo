a6d11e509d35dd70782cdf47bcbbbcbe1e6ae02a
ming <3133309264@qq.com>
2019-05-22 10:37:50 +0800

    功能基本实现

--- app/src/main/java/com/ming/sister/MainActivity.java
+++ app/src/main/java/com/ming/sister/MainActivity.java
@@ -4,137 +4,81 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
-import android.graphics.Color;
-import android.hardware.Camera;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.widget.ImageView;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.widget.VideoView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.request.RequestOptions;
-import com.ming.sister.video.GlideCircleWithBorder;
+import com.ming.sister.video.VideoActivity;
 
-import java.io.IOException;
+import java.io.File;
+import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback{
 
-    private ImageView iv_pause;
-    private ImageView iv_shrink;
-    private ImageView iv_stop;
-    private ImageView iv_change;
+public class MainActivity extends AppCompatActivity{
 
-    private Camera camera;
-    private SurfaceView sfv_camera;
-    private SurfaceHolder holder;
+
+    private ListView lv_people;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> data = new ArrayList<>();
+    private File file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //TODO
-        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},1);
-        getCamera();
-
-
         setContentView(R.layout.activity_main);
 
-        init();
-        initData();
-
-    }
-
-    private void init() {
-
-        iv_shrink = (ImageView)findViewById(R.id.iv_shrink);
-        iv_pause = (ImageView)findViewById(R.id.iv_pause);
-        iv_stop = (ImageView)findViewById(R.id.iv_stop);
-        iv_change = (ImageView)findViewById(R.id.iv_change);
-
-        //TODO
-        sfv_camera = (SurfaceView)findViewById(R.id.sfv_camera);
-        holder = sfv_camera.getHolder();
-        holder.addCallback(this);
-    }
-
-    private void initData() {
-        iv_shrink.setImageResource(R.drawable.shrink);
-
-        Glide.with(this).load(R.drawable.pause)
-                .apply(new RequestOptions().error(this.getResources().getDrawable(R.drawable.pause))
-                        .placeholder(R.mipmap.ic_launcher).centerCrop().diskCacheStrategy(DiskCacheStrategy.ALL)
-                        .transform(new GlideCircleWithBorder(this, 3, Color.parseColor("#ccffffff"))))
-                .into(iv_pause);
-
-        Glide.with(this).load(R.drawable.stop)
-                .apply(new RequestOptions().error(this.getResources().getDrawable(R.drawable.pause))
-                        .placeholder(R.mipmap.ic_launcher).centerCrop().diskCacheStrategy(DiskCacheStrategy.ALL)
-                        .transform(new GlideCircleWithBorder(this, 3, Color.parseColor("#ccff0000"))))
-                .into(iv_stop);
-
-        Glide.with(this).load(R.drawable.change)
-                .apply(new RequestOptions().error(this.getResources().getDrawable(R.drawable.pause))
-                        .placeholder(R.mipmap.ic_launcher).centerCrop().diskCacheStrategy(DiskCacheStrategy.ALL)
-                        .transform(new GlideCircleWithBorder(this, 3, Color.parseColor("#ccffffff"))))
-                .into(iv_change);
-    }
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},1);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},1);
 
 
+        setFile();
 
+        lv_people = (ListView)findViewById(R.id.lv_people);
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
+        lv_people.setAdapter(adapter);
 
+        getFileName();
 
-    //TODO
-    private void getCamera(){
-        new Thread(new Runnable() {
+        lv_people.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void run() {
-                try {
-                    camera= Camera.open(1);
-                }catch (Exception e){
-                    camera = null;
-                    e.printStackTrace();
-                }
-                if(holder != null){
-                    setStartPreView(camera,holder);
-                }
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String name = (String) adapter.getItem(position);
+                Intent intent = new Intent(MainActivity.this, VideoActivity.class);
+                intent.putExtra("name",name);
+                startActivity(intent);
             }
-        }).start();
-    }
-
-    private void releaseCamera(){
-        camera.release();
+        });
     }
 
 
-    private void setStartPreView(Camera camera, SurfaceHolder holder) {
-        if(camera == null){
-            setStartPreView(this.camera,holder);
-            return;
+
+    public void setFile(){
+        file = new File("/storage/emulated/0/sister");
+        if (!file.exists()){
+            boolean isSuccess = file.mkdirs();
+            Toast.makeText(MainActivity.this,"文件夹创建成功",Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(MainActivity.this,"文件夹已存在",Toast.LENGTH_LONG).show();
         }
-        try {
-            camera.setPreviewDisplay(holder);
-            camera.setDisplayOrientation(90);
-            camera.startPreview();
-        } catch (IOException e) {
-            e.printStackTrace();
+    }
+
+
+    //读取指定目录下的所有TXT文件的文件名
+    private void getFileName() {
+        String[] video = file.list();
+        for(String s : video){
+            data.add(s);
         }
-
+        adapter.notifyDataSetChanged();
     }
 
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
-        setStartPreView(camera,holder);
-    }
 
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        camera.stopPreview();
-        setStartPreView(camera,holder);
-    }
 
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-        releaseCamera();
-    }
 }
---
+++ app/src/main/java/com/ming/sister/video/VideoActivity.java
@@ -0,0 +1,167 @@
+package com.ming.sister.video;
+
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.Camera;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.ImageView;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.ming.sister.R;
+
+import java.io.IOException;
+
+public class VideoActivity extends AppCompatActivity implements SurfaceHolder.Callback{
+
+    private ImageView iv_pause;
+    private ImageView iv_shrink;
+    private ImageView iv_stop;
+    private ImageView iv_change;
+
+    private Camera camera;
+    private SurfaceView sfv_camera;
+    private SurfaceHolder holder;
+    private VideoView vv_video;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //TODO
+        getCamera();
+
+
+        setContentView(R.layout.activity_video);
+
+        init();
+        initData();
+
+    }
+
+    private void init() {
+
+        iv_shrink = (ImageView)findViewById(R.id.iv_shrink);
+        iv_pause = (ImageView)findViewById(R.id.iv_pause);
+        iv_stop = (ImageView)findViewById(R.id.iv_stop);
+        iv_change = (ImageView)findViewById(R.id.iv_change);
+
+        vv_video = (VideoView)findViewById(R.id.vv_video);
+
+        //TODO
+        sfv_camera = (SurfaceView)findViewById(R.id.sfv_camera);
+        holder = sfv_camera.getHolder();
+        holder.addCallback(this);
+    }
+
+    private void initData() {
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        vv_video.setVideoPath("/storage/emulated/0/sister/" + name);
+
+        vv_video.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                vv_video.start();
+
+            }
+        });
+        vv_video.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                finish();
+            }
+        });
+
+
+        iv_shrink.setImageResource(R.drawable.shrink);
+
+        Glide.with(this).load(R.drawable.pause)
+                .apply(new RequestOptions().error(this.getResources().getDrawable(R.drawable.pause))
+                        .placeholder(R.mipmap.ic_launcher).centerCrop().diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .transform(new GlideCircleWithBorder(this, 3, Color.parseColor("#ccffffff"))))
+                .into(iv_pause);
+
+        Glide.with(this).load(R.drawable.stop)
+                .apply(new RequestOptions().error(this.getResources().getDrawable(R.drawable.pause))
+                        .placeholder(R.mipmap.ic_launcher).centerCrop().diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .transform(new GlideCircleWithBorder(this, 3, Color.parseColor("#ccff0000"))))
+                .into(iv_stop);
+
+        Glide.with(this).load(R.drawable.change)
+                .apply(new RequestOptions().error(this.getResources().getDrawable(R.drawable.pause))
+                        .placeholder(R.mipmap.ic_launcher).centerCrop().diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .transform(new GlideCircleWithBorder(this, 3, Color.parseColor("#ccffffff"))))
+                .into(iv_change);
+    }
+
+
+
+
+
+    //TODO
+    private void getCamera(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    camera= Camera.open(1);
+                }catch (Exception e){
+                    camera = null;
+                    e.printStackTrace();
+                }
+                if(holder != null){
+                    setStartPreView(camera,holder);
+                }
+            }
+        }).start();
+    }
+
+    private void releaseCamera(){
+        camera.release();
+    }
+
+
+    private void setStartPreView(Camera camera, SurfaceHolder holder) {
+        if(camera == null){
+            setStartPreView(this.camera,holder);
+            return;
+        }
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.setDisplayOrientation(90);
+            camera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        setStartPreView(camera,holder);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+        camera.stopPreview();
+        setStartPreView(camera,holder);
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        releaseCamera();
+    }
+
+
+}
